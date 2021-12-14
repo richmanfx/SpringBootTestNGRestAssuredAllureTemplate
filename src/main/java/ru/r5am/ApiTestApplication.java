26b728ba5a40912de6a18f45ca0fadc5b2f83013
Aleksandr Jashhuk <richmanfx@yandex.ru>
2021-12-14 12:04:18 +0300

    Конфигурационные файлы для логера, конфигурационный класс Спринга, стартовый класс Спринга с main(), спринговый баннер.

--- src/main/java/ru/r5am/ApiTestApplication.java
+++ src/main/java/ru/r5am/ApiTestApplication.java
@@ -1,1 +1,22 @@
-﻿
+﻿package ru.r5am;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
+
+@Slf4j
+@SpringBootApplication(exclude = {
+        // Здесь можно исключить ненужные автоконфигурации для ускорения запуска
+        // Все автоконфигурации можно посмотреть в org.springframework.boot.autoconfigure
+        DataSourceAutoConfiguration.class,
+        ActiveMQAutoConfiguration.class,
+        H2ConsoleAutoConfiguration.class
+})
+public class ApiTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestApplication.class, args);
+    }
+}
--- src/main/java/ru/r5am/SpringConfig.java
+++ src/main/java/ru/r5am/SpringConfig.java
@@ -1,1 +1,9 @@
-﻿
+﻿package ru.r5am;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework. context. annotation. Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"ru.r5am"})
+public class SpringConfig {
+}
